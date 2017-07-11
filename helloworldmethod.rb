64a1fb4cb6e6c8d622f432a6106c31ba14d1cffc
Sheena Takkenberg <sheena.takkenberg@gmail.com>
2017-07-11 20:20:30 +1000

    adding stuff

---
+++ helloworldmethod.rb
@@ -0,0 +1,17 @@
+# This is a method
+def hello
+  puts "hello world!"
+end
+
+hello
+
+# parsing in arguments
+def hello2(anything)
+  puts anything
+end
+
+hello2 "My name is Sheena"
+
+hello2 "Welcome to the complete ruby on rails developer course"
+
+hello2 "Chekc it out, it's actually working!"
