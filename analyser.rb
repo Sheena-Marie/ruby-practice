5f679c8d20ce7a7e2a5bae7c6873b94349b98239
Sheena Takkenberg <sheena.takkenberg@gmail.com>
2017-07-12 21:33:05 +1000

    working with methods

--- analyser.rb
+++ analyser.rb
@@ -3,19 +3,19 @@
 # tell them how many letters are in their name
 # reverse their name!
 
-puts 'What is your first name?'
+# Asking for a person's name, telling them how many letters there are in the name, and then reversing it.
 
-first_name = gets.chomp
-
-puts 'What is your last name?'
-
-last_name = gets.chomp
-
-puts "Hi, #{first_name + ' ' + last_name}! Welcome to the analyser!"
-
-puts "There are #{first_name.length + last_name.length} characters in your name!"
-
-puts "Your name reversed would look like this: #{first_name.reverse + ' ' + last_name.reverse}"
+# puts 'What is your first name?'
+# first_name = gets.chomp
+#
+# puts 'What is your last name?'
+# last_name = gets.chomp
+#
+# puts "Hi, #{first_name + ' ' + last_name}! Welcome to the analyser!"
+#
+# puts "There are #{first_name.length + last_name.length} characters in your name!"
+#
+# puts "Your name reversed would look like this: #{first_name.reverse + ' ' + last_name.reverse}"
 
 puts "Please enter your first number"
 first_number = gets.chomp.to_f
@@ -23,16 +23,37 @@ first_number = gets.chomp.to_f
 puts "Please enter your second number"
 second_number = gets.chomp.to_f
 
-puts "The result of #{first_number.to_i} x #{second_number.to_i} is: "
-puts "#{first_number * second_number}"
+# methods for the analyser app
 
-# add in first_number divided by second_number. first_number - second_number. first_number mod of second_number is:
+# multiplying two user entered numbers
+def multiply(first_number, second_number)
+  first_number * second_number
+end
+
+# dividing two user entered numbers
+def divide(first_number, second_number)
+  first_number / second_number
+end
+
+# subtracting two user entered numbers
+def subtract(first_number, second_number)
+  first_number - second_number
+end
+
+# the mod of two user entered numbers
+def mod(first_number, second_number)
+  first_number % second_number
+end
+
+# The results of what the users enter.
+puts "The result of #{first_number.to_i} x #{second_number.to_i} is: "
+puts "#{multiply(first_number, second_number)}"
 
 puts "The result of #{first_number.to_i} divided by #{second_number.to_i} is: "
-puts "#{first_number / second_number}"
+puts "#{divide(first_number, second_number)}"
 
 puts "The result of #{first_number.to_i} - #{second_number.to_i} is: "
-puts "#{first_number - second_number}"
+puts "#{subtract(first_number, second_number)}"
 
 puts "The mod of #{first_number.to_i} and #{second_number.to_i} is: "
-puts "#{first_number % second_number}"
+puts "#{mod(first_number, second_number)}"
--- playground.rb
+++ playground.rb
@@ -1,9 +1,16 @@
-puts "Hello, enter your first name"
+# puts "Hello, enter your first name"
+#
+# first_name = gets.chomp
+#
+# puts "Please enter you last name"
+#
+# last_name = gets.chomp
+#
+# puts "Welcome, #{first_name + ' ' + last_name}, to the playground"
 
-first_name = gets.chomp
+def multiply(first_number, second_number)
+  first_number * second_number
+end
 
-puts "Please enter you last name"
-
-last_name = gets.chomp
-
-puts "Welcome, #{first_name + ' ' + last_name}, to the playground"
+puts multiply(5, 10)
+puts multiply(20,3)
