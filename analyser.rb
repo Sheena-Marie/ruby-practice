fd85dcf76238ee801cfaf5088b3b0fcebe86402a
Sheena Takkenberg <sheena.takkenberg@gmail.com>
2017-07-12 21:56:01 +1000

    cleaning up the code a bit

--- analyser.rb
+++ analyser.rb
@@ -45,15 +45,53 @@ def mod(first_number, second_number)
   first_number % second_number
 end
 
+puts "What do you want to do?"
+puts "1 - mulitply"
+puts "2 - divide"
+puts "3 - subtract"
+puts "4 - find the remainder"
+option = gets.chomp.to_i
+
+if option == 1
+  puts "You have chosen to multiply "
+  puts "The result of #{first_number.to_i} x #{second_number.to_i} is:"
+
+  result = multiply(first_number, second_number)
+
+elsif option == 2
+  puts "You have chosen to divide"
+  puts "The result of #{first_number.to_i} divided by #{second_number.to_i} is:"
+
+  result = divide(first_number, second_number)
+
+elsif option == 3
+  puts "You have chosen to subtract"
+  puts "The result of #{first_number.to_i} - #{second_number.to_i} is:"
+
+  result = subtract(first_number, second_number)
+
+elsif option == 4
+  puts "You have chosen to find the remainder"
+  puts "The remainder of #{first_number.to_i} and #{second_number.to_i} is:"
+
+  result = mod(first_number, second_number)
+  
+else
+  puts "Incorrect option. Please choose again"
+end
+
+puts result
+
+
 # The results of what the users enter.
-puts "The result of #{first_number.to_i} x #{second_number.to_i} is: "
-puts "#{multiply(first_number, second_number)}"
-
-puts "The result of #{first_number.to_i} divided by #{second_number.to_i} is: "
-puts "#{divide(first_number, second_number)}"
-
-puts "The result of #{first_number.to_i} - #{second_number.to_i} is: "
-puts "#{subtract(first_number, second_number)}"
-
-puts "The mod of #{first_number.to_i} and #{second_number.to_i} is: "
-puts "#{mod(first_number, second_number)}"
+# puts "The result of #{first_number.to_i} x #{second_number.to_i} is: "
+# puts "#{multiply(first_number, second_number)}"
+#
+# puts "The result of #{first_number.to_i} divided by #{second_number.to_i} is: "
+# puts "#{divide(first_number, second_number)}"
+#
+# puts "The result of #{first_number.to_i} - #{second_number.to_i} is: "
+# puts "#{subtract(first_number, second_number)}"
+#
+# puts "The mod of #{first_number.to_i} and #{second_number.to_i} is: "
+# puts "#{mod(first_number, second_number)}"
